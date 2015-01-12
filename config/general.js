16eeb3b069b2a229f4deb14157520662b88f4531
sergicollado <sergicollado@gmail.com>
2015-01-12 13:03:26 +0100

    warning time increased

--- config/general.js
+++ config/general.js
@@ -1,7 +1,7 @@
 module.exports = {
 
   'notifications' : {
-    enabled: true, //if disabled, no notifications will be sent
+    enabled: false, //if disabled, no notifications will be sent
     to: ['your-email@domain.com'], //default notification list if no alert_to is specified for host or url
     postmark : {
       from: 'your-email@domain.com',
